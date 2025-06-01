18f151709dc874cad9c7ef36ccabe23f5a7d53c1
elitescouter <elitescouter@gmail.com>
2025-06-01 13:52:09 -0700

    Update version to 1.19.2-1.0.3 in build.gradle and ForgeHolograms.java for consistency.

--- forge19/src/main/java/com/strictgaming/elite/holograms/forge/ForgeHolograms.java
+++ forge19/src/main/java/com/strictgaming/elite/holograms/forge/ForgeHolograms.java
@@ -39,7 +39,7 @@ import java.io.IOException;
 public class ForgeHolograms {
 
     public static final String MOD_ID = "eliteholograms";
-    public static final String VERSION = "1.19.2-1.0.4";
+    public static final String VERSION = "1.19.2-1.0.3";
     private static final Logger LOGGER = LogManager.getLogger("EliteHolograms");
 
     private static ForgeHolograms instance;
